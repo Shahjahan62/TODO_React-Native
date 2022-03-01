9486cf961a661444c0c56a60e277e296fe3fab97
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-03-01 14:04:41 -0800

    TODO app

---
+++ components/Task.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const Task = () => {
+  return (
+    <View>
+      <Text>This is a task</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Task;
